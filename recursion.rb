b38bdcb2669bac6dead813b8a9d19a977c317a54
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-01-25 14:06:58 -0500

    fib

--- recursion.rb
+++ recursion.rb
@@ -11,6 +11,23 @@ end
 
 def exp(b, n)
   return 1 if n == 0 
-  return -n/(b * 1.0) if n < 0
+  return nil if n < 0
   b * exp(b, n - 1)
+end
+
+class Array
+  def self.dup(arr)
+
+  end
+end
+
+def iter_fib(n)
+  (0..n).inject { |sum, n| sum + n }
+end
+
+def rec_fib(n)
+  return nil if n < 0
+  return [0, 1].take(n) if n <= 2
+  prev_row = rec_fib(n - 1)
+  prev_row << prev_row[-1] + prev_row[-2]
 end
